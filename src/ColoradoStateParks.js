29a124e75b74b399e138cbdf08a38abcc7909914
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-07-28 07:14:36 +0300

    Done with assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,26 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  const numberOfParks = howManyParks();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <div>
+        <h1>Colorado State Parks!</h1>
+      </div>
+      <MesaVerde />
+      <div>
+        {/* Display the values of `trees` and `wildlife` */}
+        <p>{trees}</p>
+        <p>{wildlife()}</p> {/* Call the function to log the wildlife to the console */}
+      </div>
+      {/* Display the number of parks */}
+      <p>{numberOfParks}</p>
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
